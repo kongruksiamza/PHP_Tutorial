f389290f616ed2e792c4e105190a913de2442b04
kongruksiamza <kongruksiamza123@gmail.com>
2016-11-19 20:52:21 +0700

    Add files via upload

---
+++ Shared.php
@@ -0,0 +1,16 @@
+<?php
+$score = $_GET["score"];
+$scorex = explode(":", $score);
+$highscore = $scorex[0];
+?>
+<html>
+<head>
+<title>Animal Crush Game</title>
+<meta property="og:title" content="HighScore Animal Crush is <?echo $highscore;?>!"/>
+<meta property="og:image" content="https://3.bp.blogspot.com/-gXwcFmodAJg/WDBT0VQgnyI/AAAAAAAAA9Q/fHoULsIubUQEE_e_bkLpVkWbb2OM-UAYACLcB/s1600/Poster.png"/>
+<meta property="og:site_name" content="KongRuksiam Studio"/>
+<meta property="og:description" content="<?echo $highscore;?> is mine new highscore on Animal Crush!"/>
+</head>
+<body>
+</body>
+</html>
